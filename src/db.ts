b6270117290828cf238bc46b7633dc3d7d457356
tdhoor <info@thomasdorfer.com>
2023-04-11 15:54:33 +0200

    updated .env variables

--- src/db.ts
+++ src/db.ts
@@ -7,14 +7,21 @@ import { Order } from './entity/order.entity';
 import { ProductCategory } from './entity/product-category.entity';
 import { Product } from './entity/product.entity';
 
-export const DB = new Sequelize(process.env.POSTGRES_DB_NAME, process.env.POSTGRES_DB_USER, process.env.POSTGRES_DB_PASSWORD, {
-    models: [Address, Customer, OrderItem, Order, ProductCategory, Product],
-    dialect: process.env.POSTGRES_DB_TYPE as Dialect,
-    host: process.env.POSTGRES_DB_HOST,
-    port: +process.env.POSTGRES_DB_PORT,
-    logging: false,
-    pool: {
-        max: 5
+export const DB = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        models: [
+            Address, Customer, OrderItem, Order, ProductCategory, Product
+        ],
+        dialect: process.env.DB_TYPE as Dialect,
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        logging: false,
+        pool: {
+            max: 5
+        }
     }
-});
+);
 
